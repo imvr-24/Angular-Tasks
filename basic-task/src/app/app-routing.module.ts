7e206a16d50e1fe53d3a31ee19e5abb896b806e1
imvr-24 <veerareddy596@gmail.com>
2019-07-01 11:45:29 +0530

    Added header and Routing

--- basic-task/src/app/app-routing.module.ts
+++ basic-task/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FillDetailsComponent } from './fill-details/fill-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FillDetailsComponent
+  },
+  {
+    path: 'home',
+    component: FillDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- basic-task/src/app/app.module.ts
+++ basic-task/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShowDetailsComponent } from './show-details/show-details.component';
 import { FillDetailsComponent } from './fill-details/fill-details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowDetailsComponent,
-    FillDetailsComponent
+    FillDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
